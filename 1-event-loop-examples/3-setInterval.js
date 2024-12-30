04aa935a06a0cd4a7c0a4337fa32c430ac928ded
maxkulbako <mkulbako@gmail.com>
2024-12-30 11:29:22 +0100

    сhanged project structure

--- 1-event-loop-examples/3-setInterval.js
+++
@@ -1,6 +0,0 @@
-setInterval(() => {
-  console.log("hello world");
-}, 2000);
-console.log("I will run first");
-//process stays alive until we stop it
-//ctrl + c to stop it
--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  console.log("user hit the server");
-  res.end("Welcome");
-});
-
-server.listen(3000);
