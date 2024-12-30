fb4262c9a2ee59b6a7fb6be7b451dede7b663f69
maxkulbako <mkulbako@gmail.com>
2024-12-30 16:03:33 +0100

    middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,25 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
+const logger = require("./logger");
+const authorize = require("./authorize");
+app.use([logger, authorize]);
 
-// setup static and middleware
-app.use(express.static("./public"));
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page</h1>");
+});
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-//   adding to static assets
-//   SSR
-// })
+app.get("/about", (req, res) => {
+  res.send("<h1>About Page</h1>");
+});
 
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found");
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+
+app.get("/api/items", (req, res) => {
+  res.send("Items");
 });
 
 app.listen(3000, () => {
-  console.log("server is listening on port 5000....");
+  console.log("Server is listening on port 3000...");
 });
