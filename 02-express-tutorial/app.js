98b6eea73957e807e2a1c923810547772af4967a
maxkulbako <mkulbako@gmail.com>
2024-12-31 12:27:53 +0100

    methods

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,23 +1,71 @@
 const express = require("express");
 const app = express();
-const logger = require("./logger");
-const authorize = require("./authorize");
-app.use([logger, authorize]);
+const { people } = require("./data");
 
-app.get("/", (req, res) => {
-  res.send("<h1>Home Page</h1>");
+//static assets
+app.use(express.static("./methods-public"));
+//parse form data
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
-app.get("/about", (req, res) => {
-  res.send("<h1>About Page</h1>");
+app.post("/api/people", (req, res) => {
+  console.log(req.body);
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please Provide Name Value" });
+  }
+  res.status(201).json({ success: true, person: name });
 });
 
-app.get("/api/products", (req, res) => {
-  res.send("Products");
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  console.log(id, name);
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `No person with id ${id}` });
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
 });
 
-app.get("/api/items", (req, res) => {
-  res.send("Items");
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please Provide Credentials");
+});
+
+app.delete("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `No person with id ${id}` });
+  }
+  const index = people.findIndex((person) => person.id === Number(id));
+  if (index !== -1) {
+    people.splice(index, 1);
+  }
+  res.status(200).json({ success: true, data: people });
 });
 
 app.listen(3000, () => {
